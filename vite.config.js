81043a50af5e21ee3661ff21611f00fd94c7a406
adityapryg <aditya@brainmatics.com>
2025-09-16 11:16:23 +0700

    init react

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        react(),
         tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
     ],
+    esbuild: {
+        jsx: 'automatic',
+    },
 });
